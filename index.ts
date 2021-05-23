36f36c3101e3c28bc918a42f33c16dd9229e5efa
Marius Andra <marius.andra@gmail.com>
2021-05-23 09:10:09 +0200

    better timestamp handling, use config vars

--- index.ts
+++ index.ts
@@ -5,45 +5,61 @@ declare var posthog: {
 }
 
 type SessionTrackerPlugin = Plugin<{
+    config: {
+        sessionLength: string
+        sessionStartEvent: string
+        sessionEndEvent: string
+    }
+    global: {
+        sessionLength: number
+        sessionStartEvent: string
+        sessionEndEvent: string
+    }
     jobs: {
         checkIfSessionIsOver: { distinct_id: string }
     }
 }>
 
-const SESSION_LENGTH_MINUTES = 30
-const SESSION_START_EVENT = 'session start'
-const SESSION_END_EVENT = 'session end'
+export const setupPlugin: SessionTrackerPlugin['setupPlugin'] = ({ global, config }) => {
+    global.sessionLength = parseInt(config.sessionLength) || 30
+    global.sessionStartEvent = config.sessionStartEvent || 'Session start'
+    global.sessionEndEvent = config.sessionEndEvent || 'Session end'
+}
 
-export const onEvent: SessionTrackerPlugin['onEvent'] = async (event, { cache, jobs }) => {
+export const onEvent: SessionTrackerPlugin['onEvent'] = async (event, { cache, global, jobs }) => {
     // skip this for the session start/end events
-    if (event.event === SESSION_START_EVENT || event.event === SESSION_END_EVENT) {
+    if (event.event === global.sessionStartEvent || event.event === global.sessionEndEvent) {
         return
     }
-    // check if we're the first one to increment this key in the last ${SESSION_LENGTH_MINUTES} minutes
+    // check if we're the first one to increment this key in the last ${global.sessionLength} minutes
     if ((await cache.incr(`session_${event.distinct_id}`)) === 1) {
         // if so, dispatch a session start event
-        posthog.capture(SESSION_START_EVENT, { distinct_id: event.distinct_id, timestamp: event.timestamp })
+        posthog.capture(global.sessionStartEvent, { distinct_id: event.distinct_id, timestamp: event.timestamp })
         // and launch a job to check in 30min if the session is still alive
-        await jobs.checkIfSessionIsOver({ distinct_id: event.distinct_id }).runIn(SESSION_LENGTH_MINUTES, 'minutes')
+        await jobs.checkIfSessionIsOver({ distinct_id: event.distinct_id }).runIn(global.sessionLength, 'minutes')
     }
-    // make the key expire in ${SESSION_LENGTH_MINUTES} min
-    await cache.expire(`session_${event.distinct_id}`, SESSION_LENGTH_MINUTES * 60)
-    await cache.set(`last_timestamp_${event.distinct_id}`, event.timestamp)
+    // make the key expire in ${global.sessionLength} min
+    await cache.expire(`session_${event.distinct_id}`, global.sessionLength * 60)
+    await cache.set(
+        `last_timestamp_${event.distinct_id}`,
+        event.timestamp || event.now || event.sent_at || new Date().toISOString()
+    )
 }
 
 export const jobs: SessionTrackerPlugin['jobs'] = {
     // a background job to check if a session is still in progress
-    checkIfSessionIsOver: async ({ distinct_id }, { jobs, cache }) => {
+    checkIfSessionIsOver: async ({ distinct_id }, { jobs, cache, global }) => {
         // check if there's a key that has not expired
         const ping = await cache.get(`session_${distinct_id}`, null)
         if (!ping) {
             // if it expired, dispatch the session end event
             const timestamp = await cache.get(
                 `last_timestamp_${distinct_id}`,
-                new Date(new Date().valueOf() - SESSION_LENGTH_MINUTES * 60000).toISOString()
+                new Date(new Date().valueOf() - global.sessionLength * 60000).toISOString()
             )
-            await cache.set(`last_timestamp_${distinct_id}`, null)
-            posthog.capture(SESSION_END_EVENT, { distinct_id, timestamp: timestamp })
+
+            await cache.set(`last_timestamp_${distinct_id}`, undefined)
+            posthog.capture(global.sessionEndEvent, { distinct_id, timestamp })
         } else {
             // if the key is still there, check again in a minute
             jobs.checkIfSessionIsOver({ distinct_id }).runIn(1, 'minute')
